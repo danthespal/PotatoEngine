4c7cfbe5ab3403a96d2d6cfeb379c909ac093bb7
danthespal <danielplanteskole@gmail.com>
2024-07-01 19:16:18 +0300

    Implement creating new game projects
    - Project Validation
    - Display Validation
    - Creating the Project
    - Generating Project File
    - Project Class
    - Scene Class
    - Serialization
    - Generating the Project
    - OpenProject Class

--- PotatoEngine-LevelEditor/GameProject/NewProject.cs
+++ PotatoEngine-LevelEditor/GameProject/NewProject.cs
@@ -34,7 +34,7 @@ namespace PotatoEngine_LevelEditor.GameProject
     class NewProject : ViewModelBase
     {
         // TODO: get the path from the installation location
-        private readonly string _templatePath = @"..\..\PotatoEngine-LevelEditor\ProjectTemplates";
+        private readonly string _templatePath = @"..\..\PotatoEngine-LevelEditor\ProjectTemplates\";
         private string _projectName = "NewProject";
         public string ProjectName
         {
@@ -44,6 +44,7 @@ namespace PotatoEngine_LevelEditor.GameProject
                 if (_projectName != value)
                 {
                     _projectName = value;
+                    ValidateProjectPath();
                     OnPropertyChanged(nameof(ProjectName));
                 }
             }
@@ -58,7 +59,36 @@ namespace PotatoEngine_LevelEditor.GameProject
                 if (_projectPath != value)
                 {
                     _projectPath = value;
+                    ValidateProjectPath();
                     OnPropertyChanged(nameof(ProjectPath));
+                }
+            }
+        }
+
+        private bool _isValid;
+        public bool IsValid
+        {
+            get => _isValid;
+            set
+            {
+                if (_isValid != value)
+                {
+                    _isValid = value;
+                    OnPropertyChanged(nameof(IsValid));
+                }
+            }
+        }
+
+        private string _errorMsg;
+        public string ErrorMsg
+        {
+            get => _errorMsg;
+            set
+            {
+                if (_errorMsg != value)
+                {
+                    _errorMsg = value;
+                    OnPropertyChanged(nameof(ErrorMsg));
                 }
             }
         }
@@ -66,6 +96,80 @@ namespace PotatoEngine_LevelEditor.GameProject
         private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
         public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates
         { get; }
+
+        private bool ValidateProjectPath()
+        {
+            var path = ProjectPath;
+            if (!Path.EndsInDirectorySeparator(path)) path += @"\";
+            path += $@"{ProjectName}\";
+
+            IsValid = false;
+            if (string.IsNullOrWhiteSpace(ProjectName.Trim()))
+            {
+                ErrorMsg = "Type in a project name";
+            }
+            else if (ProjectName.IndexOfAny(Path.GetInvalidFileNameChars()) != -1)
+            {
+                ErrorMsg = "Invalid character(s) used in project name.";
+            }
+            else if (string.IsNullOrWhiteSpace(ProjectPath.Trim()))
+            {
+                ErrorMsg = "Select a valid project folder.";
+            }
+            else if (ProjectPath.IndexOfAny(Path.GetInvalidPathChars()) != -1)
+            {
+                ErrorMsg = "Invalid character(s) used in project path.";
+            }
+            else if (Directory.Exists(path) && Directory.EnumerateFileSystemEntries(path).Any())
+            {
+                ErrorMsg = "Selected project folder already exists and is not empty.";
+            }
+            else
+            {
+                ErrorMsg = "";
+                IsValid = true;
+            }
+
+            return IsValid;
+        }
+
+        public string CreateProject(ProjectTemplate template)
+        {
+            ValidateProjectPath();
+            if (!IsValid)
+            {
+                return string.Empty;
+            }
+
+            if (!Path.EndsInDirectorySeparator(ProjectPath)) ProjectPath += @"\";
+            var path = $@"{ProjectPath}{ProjectName}\";
+
+            try
+            {
+                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+                foreach (var folder in template.Folders)
+                {
+                    Directory.CreateDirectory(Path.GetFullPath(Path.Combine(Path.GetDirectoryName(path), folder)));
+                }
+                var dirInfo = new DirectoryInfo(path + @".Potato\");
+                dirInfo.Attributes |= FileAttributes.Hidden;
+                File.Copy(template.IconFilePath, Path.GetFullPath(Path.Combine(dirInfo.FullName, "Icon.png")));
+                File.Copy(template.ScreenshotFilePath, Path.GetFullPath(Path.Combine(dirInfo.FullName, "Screenshot.png")));
+
+                var projectXml = File.ReadAllText(template.ProjectFilePath);
+                projectXml = string.Format(projectXml, ProjectName, ProjectPath);
+                var projectPath = Path.GetFullPath(Path.Combine(path, $"{ProjectName}{Project.Extension}"));
+                File.WriteAllText(projectPath, projectXml);
+
+                return path;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                // TODO: log error
+                return string.Empty;
+            }
+        }
 
         public NewProject()
         {
@@ -85,6 +189,7 @@ namespace PotatoEngine_LevelEditor.GameProject
 
                     _projectTemplates.Add(template);
                 }
+                ValidateProjectPath();
             }
             catch (Exception ex)
             {
--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-# PotatoEngine
- PotatoEngine is a low level C++ game engine
+# PotatoEngine [alhpa-stage]
+ **PotatoEngine** is a low level C++ game engine
+<br />
+<br />
+## Engine Structure
+```mermaid
+graph LR
+A[PotatoEngine-Lib C++] -- DLL --> B[PotatoEngine-LevelEditor WPF]
+B --> D{GameCode C++}
+A --> D
+```
+<br />
+## Project Structure
+```mermaid
+graph LR
+A[Project] --> B[Scenes]
+B --> C[GameEntities]
+C --> D[Components]
+```
+**Project** - central data structure, contains everything.<br />
+**Scenes** - Is a game level or a chunk of the world.
 
-
-# Engine Diagram
-PotatoEngine-Lib (C++) <- DLL -> PotatoEngine-LevelEditor (WPF)
-GameCode (C++)
