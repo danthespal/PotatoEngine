a22d0bf45e739ffb3b5378cc5c7eee2bb899e520
danthespal <danielplanteskole@gmail.com>
2024-07-03 00:15:39 +0300

    finishing the LoggerView
    just a basic filter system and clear

--- PotatoEngine-LevelEditor/Utilities/LoggerView.xaml.cs
+++ PotatoEngine-LevelEditor/Utilities/LoggerView.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PotatoEngine_LevelEditor.Utilities
 {
@@ -23,6 +22,27 @@ namespace PotatoEngine_LevelEditor.Utilities
         public LoggerView()
         {
             InitializeComponent();
+
+            Loaded += (s, e) =>
+            {
+                Logger.Log(MessageType.Info, "Information message");
+                Logger.Log(MessageType.Warning, "Warning message");
+                Logger.Log(MessageType.Error, "Error message");
+            };
+        }
+
+        private void OnClear_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Logger.Clear();
+        }
+
+        private void OnMessageFilter_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var filter = 0x0;
+            if (toggleInfo.IsChecked == true) filter |= (int)MessageType.Info;
+            if (toggleWarnings.IsChecked == true) filter |= (int)MessageType.Warning;
+            if (toggleErrors.IsChecked == true) filter |= (int)MessageType.Error;
+            Logger.SetMessageFilter(filter);
         }
     }
 }
