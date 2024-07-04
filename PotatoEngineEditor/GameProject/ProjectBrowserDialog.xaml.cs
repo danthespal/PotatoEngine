8164c5ecd1f048e4aa5340287587bbdf4c2f5edb
danthespal <danielplanteskole@gmail.com>
2024-07-04 23:38:03 +0300

    Editor: Dark UI theme and animations
    - setting up the color palette
    - window style
    - change the WindowChrome
    - window template
    - close/minimize/maximize buttons
    - button style
    - dialog style
    - project browser toggle buttons style
    - new project style
    - TextBlock style
    - TextBox style
    - open project style
    - animate project browser transition

--- PotatoEngineEditor/GameProject/ProjectBrowserDialog.xaml.cs
+++ PotatoEngineEditor/GameProject/ProjectBrowserDialog.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -17,6 +18,8 @@ namespace PotatoEngineEditor.GameProject
     /// </summary>
     public partial class ProjectBrowserDialog : Window
     {
+        private readonly CubicEase _easing = new CubicEase() { EasingMode = EasingMode.EaseInOut };
+
         public ProjectBrowserDialog()
         {
             InitializeComponent();
@@ -34,6 +37,32 @@ namespace PotatoEngineEditor.GameProject
             }
         }
 
+        private void AnimateToCreateProject()
+        {
+            var highlightAnimation = new DoubleAnimation(200, 400, new Duration(TimeSpan.FromSeconds(0.2)));
+            highlightAnimation.EasingFunction = _easing;
+            highlightAnimation.Completed += (s, e) =>
+            {
+                var animation = new ThicknessAnimation(new Thickness(0), new Thickness(-1600, 0, 0, 0), new Duration(TimeSpan.FromSeconds(0.5)));
+                animation.EasingFunction = _easing;
+                browserContent.BeginAnimation(MarginProperty, animation);
+            };
+            highlightRect.BeginAnimation(Canvas.LeftProperty, highlightAnimation);
+        }
+
+        private void AnimateToProject()
+        {
+            var highlightAnimation = new DoubleAnimation(400, 200, new Duration(TimeSpan.FromSeconds(0.2)));
+            highlightAnimation.EasingFunction = _easing;
+            highlightAnimation.Completed += (s, e) =>
+            {
+                var animation = new ThicknessAnimation(new Thickness(-1600, 0, 0, 0), new Thickness(0), new Duration(TimeSpan.FromSeconds(0.5)));
+                animation.EasingFunction= _easing;
+                browserContent.BeginAnimation(MarginProperty, animation);
+            };
+            highlightRect.BeginAnimation(Canvas.LeftProperty, highlightAnimation);
+        }
+
         private void OnToggleButton_Click(object sender, RoutedEventArgs e)
         {
             if (sender == openProjectButton)
@@ -41,7 +70,9 @@ namespace PotatoEngineEditor.GameProject
                 if (createProjectButton.IsChecked == true)
                 {
                     createProjectButton.IsChecked = false;
-                    browserContent.Margin = new Thickness(0);
+                    AnimateToProject();
+                    openProjectView.IsEnabled = true;
+                    newProjectView.IsEnabled = false;
                 }
                 openProjectButton.IsChecked = true;
             }
@@ -50,7 +81,9 @@ namespace PotatoEngineEditor.GameProject
                 if (openProjectButton.IsChecked == true)
                 {
                     openProjectButton.IsChecked = false;
-                    browserContent.Margin = new Thickness(-800, 0, 0, 0);
+                    AnimateToCreateProject();
+                    openProjectView.IsEnabled = false;
+                    newProjectView.IsEnabled = true;
                 }
                 createProjectButton.IsChecked = true;
             }
