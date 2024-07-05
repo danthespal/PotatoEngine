63407d8ffa95e87fe4373d4181860bbb95ba1c5c
danthespal <danielplanteskole@gmail.com>
2024-07-05 22:49:55 +0300

    Engine: scripting and script component
    - base class for game code classes
    - script component for game entities

--- PotatoEngine/Components/Transform.cpp
+++ PotatoEngine/Components/Transform.cpp
@@ -12,7 +12,7 @@ utl::vector<math::v3> scales;
 } // anonymous namespaces
 
 component
-create_transform(const init_info& info, game_entity::entity entity)
+create(init_info info, game_entity::entity entity)
 {
 	assert(entity.is_valid());
 	const id::id_type entity_index{ id::index(entity.get_id()) };
@@ -35,7 +35,7 @@ create_transform(const init_info& info, game_entity::entity entity)
 }
 
 void
-remove_transform(component c)
+remove(component c)
 {
 	assert(c.is_valid());
 }
--- PotatoEngine/Utilities/Utilities.h
+++ PotatoEngine/Utilities/Utilities.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+
 #define USE_STL_VECTOR 1
 #define USE_STL_DEQUE 1
 
@@ -8,6 +10,20 @@
 namespace PotatoEngine::utl {
 template<typename T>
 using vector = std::vector<T>;
+
+template<typename T>
+void erase_unordered(std::vector<T>& v, size_t index)
+{
+	if (v.size() > 1)
+	{
+		std::iter_swap(v.begin() + index, v.end() - 1);
+		v.pop_back();
+	}
+	else
+	{
+		v.clear();
+	}
+}
 }
 #endif
 
