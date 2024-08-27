1a7b16da33b0f40dfdf3089421f9d9554ad8b499
danthespal <danielplanteskole@gmail.com>
2024-08-27 23:55:48 +0300

    generating a plane grid procedurally

--- PotatoEngine/Common/CommonHeaders.h
+++ PotatoEngine/Common/CommonHeaders.h
@@ -13,6 +13,8 @@
 #endif
 
 // common headers
+#include "PrimitiveTypes.h"
+#include "..\Utilities\Math.h"
 #include "..\Utilities\Utilities.h"
 #include "..\Utilities\MathTypes.h"
 #include "PrimitiveTypes.h"
