8adf1572503a1cc5751757a4610fed1fbd44956c
danthespal <danielplanteskole@gmail.com>
2024-09-03 19:49:54 +0300

    geometry asset class

---
+++ PotatoEngineEditor/Content/Asset.cs
@@ -0,0 +1,32 @@
+﻿using PotatoEngineEditor.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotatoEngineEditor.Content
+{
+    enum AssetType
+    {
+        Unknown,
+        Animation,
+        Audio,
+        Material,
+        Mesh,
+        Skeleton,
+        Texture,
+    }
+
+    abstract class Asset : ViewModelBase
+    {
+        public AssetType Type { get; private set; }
+
+        public Asset(AssetType type)
+        {
+            Debug.Assert(type != AssetType.Unknown);
+            Type = type;
+        }
+    }
+}
