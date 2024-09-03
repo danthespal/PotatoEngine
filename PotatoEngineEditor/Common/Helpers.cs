f8c1fca238609cbb9c2ce5e6daa909dc149d9150
danthespal <danielplanteskole@gmail.com>
2024-09-03 21:04:32 +0300

    reading data into the geometry asset class

--- PotatoEngineEditor/Common/Helpers.cs
+++ PotatoEngineEditor/Common/Helpers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,22 @@ namespace PotatoEngineEditor
                 parent = VisualTreeHelper.GetParent(parent);
             }
             return null;
+        }
+    }
+
+    public static class ContentHelper
+    {
+        public static string GetRandomString(int length = 8)
+        {
+            if (length <= 0) length = 8;
+            var n = length / 11;
+            var sb = new StringBuilder();
+            for (int i = 0; i <= n; ++i)
+            {
+                sb.Append(Path.GetRandomFileName().Replace(".", ""));
+            }
+
+            return sb.ToString(0, length);
         }
     }
 }
