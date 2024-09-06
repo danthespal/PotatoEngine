71bed9b0f9ce14d7f3844108dcbd4d4515a5bf54
danthespal <danielplanteskole@gmail.com>
2024-09-06 14:01:39 +0300

    initializing DirectX12

--- PotatoEngine/Graphics/Direct3D12/D3D12Core.h
+++ PotatoEngine/Graphics/Direct3D12/D3D12Core.h
@@ -6,4 +6,14 @@ namespace PotatoEngine::graphics::d3d12::core {
 bool initialize();
 void shutdown();
 
+template<typename T>
+constexpr void release(T*& resource)
+{
+	if (resource)
+	{
+		resource->Release();
+		resource = nullptr;
+	}
+}
+
 }
--- PotatoEngine/Graphics/Renderer.cpp
+++ PotatoEngine/Graphics/Renderer.cpp
@@ -1,5 +1,6 @@
 #include "Renderer.h"
 #include "GraphicsPlatformInterface.h"
+#include "Direct3D12/D3D12Interface.h"
 
 namespace PotatoEngine::graphics {
 namespace {
@@ -15,8 +16,9 @@ set_platform_interface(graphics_platform platform)
 		d3d12::get_platform_interface(gfx);
 		break;
 	default:
-		break;
+		return false;
 	}
+	return true;
 }
 
 } // anonymous namespace
@@ -24,7 +26,7 @@ set_platform_interface(graphics_platform platform)
 bool
 initialize(graphics_platform platform)
 {
-	return set_platform_interface(platform);
+	return set_platform_interface(platform) && gfx.initialize();
 }
 
 void
