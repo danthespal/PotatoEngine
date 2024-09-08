f021bbfe20acf57bdd9030bdbbcb0ea40f5e9b3e
danthespal <danielplanteskole@gmail.com>
2024-09-08 22:48:46 +0300

    DirectX (DXGI) Swap Chain

--- PotatoEngine/Graphics/Direct3D12/D3D12Core.h
+++ PotatoEngine/Graphics/Direct3D12/D3D12Core.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "D3D12CommonHeaders.h"
 
+namespace PotatoEngine::graphics::d3d12 {
+class descriptor_heap;
+}
+
 namespace PotatoEngine::graphics::d3d12::core {
 
 bool initialize();
@@ -32,6 +36,11 @@ constexpr void deferred_release(T*& resource)
 }
 
 ID3D12Device *const device();
+descriptor_heap& rtv_heap();
+descriptor_heap& dsv_heap();
+descriptor_heap& srv_heap();
+descriptor_heap& uav_heap();
+DXGI_FORMAT default_render_target_format();
 u32 current_frame_index();
 void set_deferred_releases_flag();
 
